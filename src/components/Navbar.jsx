d99568e062a0615fea6da5bbb45a3ed6c6380787
verstka1@art-web.ru <verstka1@art-web.ru>
2024-08-22 09:29:19 +0300

    Создание меню

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 const Navbar = () => {
     const [active, setActive] = useState("");
+    const [toggle, setToggle] = useState(false);
     return (
         <nav
             className={`
@@ -24,28 +25,35 @@ const Navbar = () => {
                     <img
                         src={logo}
                         alt="logo"
-                        className="w-28 g-28 object-contain"
+                        className="w-20 g-20 object-contain"
                     />
                     <p className="text-white text-[18px] font-bold">
-                        Разработка веб-сайтов
+                        Разработчик веб-сайтов
                     </p>
                 </Link>
                 <ul className="list-none hidden sm:flex flex-row gap-10">
-                    {navLinks.map((link) => {
+                    {navLinks.map((link) => (
                         <li
                             key={link.id}
                             className={`${
-                                active === link.title 
-                                ? "text-white"
-                                : "text-secondary"
+                                active === link.title
+                                    ? "text-white"
+                                    : "text-secondary"
                             } hover:text-white text-[18px] font-medium cursor-pointer`}
+                            onClick={() => {
+                                setActive(link.title);
+                            }}
                         >
-                            <a href={`#${link.id}`}>
-                                {link.title}
-                            </a>
+                            <a href={`#${link.id}`}>{link.title}</a>
                         </li>
-                    })}
+                    ))}
                 </ul>
+
+                <button className="block w-16 h-8 relative  " onClick={() => setToggle(!toggle)}>
+                    <span className={`bg-white  h-0.5 absolute left-0  transition-all origin-center ${toggle ? "rotate-45 w-full top-1/2" : "top-0 w-1/2"}`}></span>
+                    <span className={`bg-white w-full h-0.5 absolute left-0 top-1/2 transition-all origin-center ${toggle ? "" : ""}`}></span>
+                    <span className={`bg-white  h-0.5 absolute right-0 bottom-0 transition-all origin-center ${toggle ? "-rotate-45 w-full top-1/2" : "w-1/2"}`}></span>
+                </button>
             </div>
         </nav>
     );
