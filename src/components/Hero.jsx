10824bb565c5dac1c773fa613c30955228390a10
Tim <topwebknurl@gmail.com>
2024-08-29 14:53:46 +0300

    добавление и вывод 3d модели в блок Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,27 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>Hero</section>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w7xl flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justyfy-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Привет, меня зовут  <span className="text-[#915eff]">Тимур</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            💻 Я — веб-разработчик, создающий уникальные и функциональные веб-сайты и веб-приложения.
+            <br className="sm:block hidden" />
+            🚀 Моя работа — превращать ваши идеи в реальность и делать интернет лучше!
+          </p>
+        </div>
+      </div>
+      
+      <ComputersCanvas />
+    </section>
   )
 }
 
