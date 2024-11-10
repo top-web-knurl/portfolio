ad197e89d4f90e413e17d2a63db9e8b17b18d04b
Tim <topwebknurl@gmail.com>
2024-11-10 15:26:16 +0300

    3d карточки обо мне

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,28 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
-const ServiceCard = ({title, ico}) => {
+const ServiceCard = ({index, title, icon}) => {
   return (
-    <div>
-      {console.log(123)}
-      {console.log(title)}
-      <p>{title}</p>
-    </div>
+    <Tilt className="xs:w-[250px] w-full">
+       <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full violet-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        >
+          <div
+           options={{
+              max: 45,
+              scale: 1,
+              speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col text-center"
+          >
+           <img src={icon} alt={title} 
+           className="w-16 h-16 object-contain"
+           />
+           <h3 className='text-white text-[20px]'>{title}</h3>
+          </div>
+       </motion.div>
+    </Tilt>
   )
 }
 const About = () => {
