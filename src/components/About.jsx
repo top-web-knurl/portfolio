9aef2152c2d53eb3bfef03406bbeb044eb9716c2
Timur <achilovtima2@gmail.com>
2025-05-10 16:57:15 +0300

    обновление three.js и её зависимостей, обновление кода под новые версии, замена react-tilt на atroposjs и обновление соостветствующего кода

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import { Tilt } from 'react-tilt'
+import Atropos from 'atropos/react';
 import { motion } from 'framer-motion'
 import SectionWrapper from '../hoc/SectionWrapper'
 import PropTypes from 'prop-types'
@@ -6,32 +6,39 @@ import PropTypes from 'prop-types'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import 'atropos/css/min'
 
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt 
-      className="xs:w-[250px] w-full"
-      options={{
-        max: 45,
-        scale: 1,
-        speed: 450
-      }}
-    >
-      <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full violet-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    <Atropos 
+      className="xs:w-[250px] w-full rounded-[20px]"
+      activeOffset={40}
+      shadowScale={1}
+      rotateXMax={20} 
+      rotateYMax={20} 
+      shadow={false} 
+      highlight={true}
+      data-rounded="inherit"
+    > 
+      <motion.div 
+        variants={fadeIn("fade", "ease", 0.5 * index, 0.75)}
+        className="w-full violet-pink-gradient p-[1px] rounded-[20px]"
       >
         <div
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col text-center"
         >
           <img src={icon} alt={title}
             className="w-16 h-16 object-contain"
+            data-atropos-offset="20"
           />
-          <h3 className='text-white text-[20px]'>{title}</h3>
+          <h3
+           className='text-white text-[20px]'
+             data-atropos-offset="15"
+           >{title}</h3>
         </div>
       </motion.div>
-    </Tilt>
+    </Atropos>
   )
 }
 
--- src/components/canvas/ComputersCanvas.jsx
+++ src/components/canvas/ComputersCanvas.jsx
@@ -1,15 +1,39 @@
-import React, { Suspense, useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import  { Suspense, useEffect, useState } from 'react'
+import { Canvas, useThree } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
+import PropTypes from 'prop-types'
+
 
 const Computers = ({ isMobail }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
+  const { viewport } = useThree()
+  const [scale, setScale] = useState(1)
+
+  useEffect(() => {
+    const updateScale = () => {
+      let newScale
+
+      if (window.innerWidth < 768) {
+        newScale = Math.max(0.3, Math.min(0.75, (window.innerWidth * 0.75) / 1000))
+      } else {
+        newScale = 0.75
+      }
+
+      setScale(newScale)
+    }
+
+    updateScale()
+    window.addEventListener('resize', updateScale)
+
+    return () => window.removeEventListener('resize', updateScale)
+  }, [viewport.factor])
+
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <spotLight
+      <directionalLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
@@ -19,12 +43,16 @@ const Computers = ({ isMobail }) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobail ? .6 : 0.75}
-        position={isMobail ? [0, -4.2, -1] : [0, -3.75, -1.5]}
+        scale={isMobail ? scale : 0.75}
+        position={isMobail ? [0, -4.2, -.5] : [0, -3.75, -1.5]}
         rotation={[-0.01, -0.2, 0]}
       />
     </mesh>
   )
+}
+
+Computers.propTypes = {
+  isMobail: PropTypes.bool
 }
 
 const ComputersCanvas = () => {
